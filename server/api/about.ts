37fb695e5310b7afc912dde96ad7875a811112ed
Gaute Meek Olsen <gaute.meek@gmail.com>
2023-05-14 22:45:02 +0200

    move openai creation

--- server/api/about.ts
+++ server/api/about.ts
@@ -1,11 +1,11 @@
 import { Configuration, OpenAIApi } from 'openai';
 
-const configuration = new Configuration({
-  apiKey: useRuntimeConfig().openAiApiKey,
-});
-const openai = new OpenAIApi(configuration);
-
 export default defineEventHandler(async (event) => {
+  const configuration = new Configuration({
+    apiKey: useRuntimeConfig().openAiApiKey,
+  });
+  const openai = new OpenAIApi(configuration);
+
   const query = getQuery(event)
   const prompt = `Look at the website ${query.site}. You are an absolut fangirl of this site. Now give a summary of what this site do and looks in the most loving and epic way.`
   const response = await openai.createCompletion({
